33c8206ee830981be06eab54da953aa12dd0ff1f
Sarah Carter <cartwellcreative@gmail.com>
2024-05-08 23:09:35 -0400

    file and folder structure setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//want to bring this into it's own layout because these screens will not have the navigation bar that the others do
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>TabsLayout</Text>
+    </View>
+  )
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
