eb69e03dec29994b6e24cff391bcdeeea97c7ce1
Sarah Carter <31831752+cartwellcreative@users.noreply.github.com>
2024-05-08 22:56:46 -0400

    app_and_assets

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//want to bring this into it's own layout because these screens will not have the navigation bar that the others do
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  )
+}
+
+export default AuthLayout
+
+const styles = StyleSheet.create({})
---
+++ app/_layout.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync(); // prevent the splash screen from hiding before asset loading is complete
+
+
+const RouteLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+      if(error) throw error;
+      if(fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error])
+
+    if(!fontsLoaded && !error) return null;
+
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={ {headerShown: false} }/>
+    </Stack>
+  )
+}
+
+export default RouteLayout
