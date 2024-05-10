8f7cfe317af121bbd1ab9721194a4142d298e9f8
Sarah Carter <cartwellcreative@gmail.com>
2024-05-09 23:17:17 -0400

    cleanup from example app

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl font-pblack">Aora!</Text>
+        <Text className="text-3xl font-pblack">Gift Giver App</Text>
         <StatusBar style="auto" />
         <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
         </View>
